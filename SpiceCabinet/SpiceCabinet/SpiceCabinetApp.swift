a0db604b0bcdb44a2208764b9f41ac48dc2048f0
Daniel L <lanieldarson@gmail.com>
2025-07-08 11:03:42 -0700

    Initial Xcode project

---
+++ SpiceCabinet/SpiceCabinet/SpiceCabinetApp.swift
@@ -0,0 +1,17 @@
+//
+//  SpiceCabinetApp.swift
+//  SpiceCabinet
+//
+//  Created by Daniel Larson on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SpiceCabinetApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SpiceCabinet/SpiceCabinetTests/SpiceCabinetTests.swift
@@ -0,0 +1,16 @@
+//
+//  SpiceCabinetTests.swift
+//  SpiceCabinetTests
+//
+//  Created by Daniel Larson on 7/8/25.
+//
+
+import Testing
+
+struct SpiceCabinetTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
